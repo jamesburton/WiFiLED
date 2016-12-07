76fb601d1e54259724ef0a1c041c0b64d2558e87
James Burton <james@code-consultants.co.uk>
2016-12-07 19:42:44 +0000

    Start of splintering into separate files, for ease of maintenance

--- WifiLED.ino
+++ WifiLED.ino
@@ -1,4 +1,5 @@
 // Version History
+// v1.6 - Added switches to toggle between Blynk and MQTT, with initial MQTT functionality
 // v1.5 - Adding ArduinoOTA, to allow remote deployment to active devices
 // v1.4 - Adding Blynk, for remote control app
 // v1.3 - Added WebServer functionality for control
@@ -6,13 +7,46 @@
 // v1.1 - Added FastLED and initial LED control
 // v1.0 - Initial Project
 
+//#include "Options.h"	// NB: Change defines here to enable/disable features/modes, vary LED count, etc.
+#define USE_BLYNK
+#define USE_MMQT
+#define USE_ARDUINO_OTA
+
+#define NUM_LEDS 30
+#define LED_DATA_PIN 1
+
+// Override, to allow playing with part of a larger strip ... otherwise, simply match the NUM_LEDS setting to your strip instead
+#define ACTIVE_LEDS NUM_LEDS
+// Override, to set an initial reduced active LED count (default should be all)
+#define NUM_COLOURS ACTIVE_LEDS
+
+#ifdef USE_BLYNK
+//#define BLYNK_AUTH_TOKEN ""
+#define BLYNK_AUTH_TOKEN "ac2c04d72f3c4d89a2d1485ba422b6dd"
+#endif  // USE_BLYNK
 
 // NB: For WifiManager info, see https://github.com/tzapu/WiFiManager
+
+const int aNumber = NUM_LEDS;
+
+#ifdef USE_BLYNK
 #define BLYNK_PRINT Serial
 #define BLYNK_MAX_SENDBYTES 256	// NB: Default is 128
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
-#define DEFAULT_PASSWORD "12345"
+
+#define BLYNK_AUTH_TOKEN "ac2c04d72f3c4d89a2d1485ba422b6dd"
+#define OFF_PIN V0
+#define TERMINAL_PIN V1
+#define RGB_PIN V2
+#define MENU_PIN V3
+#define RESTART_PIN V4
+#define LED_PIN V5
+#define BRIGHTNESS_PIN V6
+#define LIGHT_SENSOR_PIN V7
+#else  // If not USE_BLYNK
+#include <ESP8266WiFi.h>
+#endif // USE_BLYNK
 
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
@@ -24,84 +58,17 @@
 
 #include <FastLED.h>
 
+#ifdef USE_ARDUINO_OTA
 #include <ArduinoOTA.h>
+#endif // USE_ARDUINO_OTA
 
 #include <EEPROM.h>
 
-//#define NUM_LEDS 30   // NB: Works, but flickers with low light for blue if all active
-#define NUM_LEDS 32   // NB: Works, but flickers with low light for blue
-//#define NUM_LEDS 32
-//#define NUM_LEDS 16
-//#define NUM_LEDS 15
-//#define NUM_LEDS 12 // NB: Works, but flickers slightly for blue
-//#define NUM_LEDS 10 // Still flickering on blue
-//#define NUM_LEDS 6  // Flickering blue, but good enough
-//#define NUM_LEDS 8  // Flickering blue, but good enough
-//#define NUM_LEDS 6  // Solid colours
-#define ACTIVE_LEDS NUM_LEDS
-//#define ACTIVE_LEDS 24  // NB: Good solid output with mix!
-//#define ACTIVE_LEDS 28  // NB: Slightly weak blue output!
-//#define ACTIVE_LEDS 32  // NB: Pretty good output!
-//#define ACTIVE_LEDS 48
-//#define ACTIVE_LEDS 6
-//#define ACTIVE_LEDS 16
-#define NUM_COLOURS ACTIVE_LEDS // NB: This is now a maximum mix size, and a variable will set the active size
-#define DATA_PIN 1
-
-#define TERMINAL_PIN V1
-#define RGB_PIN V2
-
-#define SETTINGS_VALID 123
-#define SETTINGS_INVALID 0xFE
-#define SETTINGS_EEPROM_OFFSET 512
-struct Settings {
-  int valid;
-  CRGB colours_custom[NUM_COLOURS];
-  int colours_count;
-  int activeLEDs;
-  bool cycling;
-  bool flashing;
-  //char password;  // NB: Should consider encryption before adding this
-} settings;
-
-char blynkAuthToken[] = "ac2c04d72f3c4d89a2d1485ba422b6dd";
-WidgetTerminal terminal(TERMINAL_PIN);
-
-int activeLEDs = ACTIVE_LEDS;
-
-int numColours = NUM_COLOURS;
-//CRGB leds[NUM_LEDS];
-CRGBArray<NUM_LEDS> leds;
-CRGB colours[NUM_COLOURS];
-CRGB colours_mixed[3];
-CRGB colours_redgreen[1];
-CRGB *remote_colour;
-
-WiFiServer server(80);
-
-/*
-  enum LEDCommands {
-	None,
-	Off,
-	On,
-	Clear,
-	Red,
-	Green,
-	Blue,
-	Purple,
-	RedGreen,
-	Mixed,
-	CustomSingle,
-	CustomMixed,
-	FlashOn,
-	FlashOff,
-	CycleOn,
-	CycleOff
-  };
-*/
+//#include "Constants.cpp"
+//#include "Constants.h"
 const int COMMAND_NONE = 0;
 const int COMMAND_OFF = 1;
-//const int COMMAND_ON = 2;
+const int COMMAND_ON = 2;
 const int COMMAND_CLEAR = COMMAND_OFF;
 const int COMMAND_WHITE = 3;
 const int COMMAND_RED = 4;
@@ -125,10 +92,86 @@ const int COMMAND_RAINBOW = 20;
 const int COMMAND_CUSTOM_RAINBOW = 21;
 const int COMMAND_RESTART = 22;
 const int COMMAND_CLEAR_WIFI = 23;
+const int COMMAND_SET_BRIGHTNESS = 24;
+const int COMMAND_DARKEN = 25;
+const int COMMAND_LIGHTEN = 26;
+const int COMMAND_RESET = 27;
+
+#ifdef USE_BLYNK
+const int BLYNK_MENU_OFF = 1;
+const int BLYNK_MENU_WHITE = 2;
+const int BLYNK_MENU_RAINBOW = 3;
+const int BLYNK_MENU_RED = 4;
+const int BLYNK_MENU_GREEN = 5;
+const int BLYNK_MENU_BLUE = 6;
+const int BLYNK_MENU_MIXED = 7;
+const int BLYNK_MENU_RED_GREEN = 8;
+const int BLYNK_MENU_RANDOM_RAINBOW = 9;
+const int BLYNK_MENU_RANDOM_RGB = 10;
+const int BLYNK_MENU_SAVE = 11;
+const int BLYNK_MENU_LOAD = 12;
+const int BLYNK_MENU_RESTART = 13;
+const int BLYNK_MENU_RESET = 14;
+const int BLYNK_MENU_CLEAR_WIFI = 15;
+//const int BLYNK_MENU_ = ;
+#endif  // #ifdef USE_BLYNK
+
+//#include "Settings.h"
+#define SETTINGS_VALID 123
+#define SETTINGS_INVALID 0xFE
+#define SETTINGS_EEPROM_OFFSET 512
+
+struct Settings {
+  int valid;
+  CRGB colours_custom[NUM_COLOURS];
+  int colours_count;
+  int activeLEDs;
+  bool cycling;
+  bool flashing;
+  //char password;  // NB: Should consider encryption before adding this
+  int lastCommand;
+  byte brightness;
+  bool on;
+  bool autoSave;
+};
+
+
+/////////////////////////////////////
+// Function Declarations
+
+void setLEDs();
+
+/////////////////////////////////////
+
+#ifdef USE_BLYNK
+char blynkAuthToken[] = BLYNK_AUTH_TOKEN;
+WidgetTerminal terminal(TERMINAL_PIN);
+#endif	// USE_BLYNK
+
+//struct Settings settings;
+
+int activeLEDs = ACTIVE_LEDS;
+
+int numColours = NUM_COLOURS;
+//CRGB leds[NUM_LEDS];
+CRGBArray<NUM_LEDS> leds;
+CRGB colours[NUM_COLOURS];
+CRGB colours_mixed[3];
+CRGB colours_redgreen[1];
+CRGB *remote_colour;
+
+WiFiServer server(80);
+
+int cycleIndex;
+int cycleCount;
+
+//String homepage;
+
+Settings settings;
 
 void setLEDs() {
   for (int i = 0; i < activeLEDs; i++) {
-    leds[i] = colours[i % numColours];
+    leds[i] = colours[cycleIndex + i % numColours];
   }
   FastLED.show();
 }
@@ -164,6 +207,7 @@ void setLEDs(struct CRGB colour) {
   FastLED.show();
 }
 
+#ifdef USE_ARDUINO_OTA
 void setup_ArduinoOTA()
 {
   // Port defaults to 8266
@@ -194,12 +238,11 @@ void setup_ArduinoOTA()
   });
   ArduinoOTA.begin();
 }
+#endif	// USE_ARDUINO_OTA
 
 String bsLink(String link) {
   return "<a class=\"btn btn-success\" href=\"" + link + "\">" + link + "</a>";
 }
-
-String homepage;
 
 void buildHomepage() {
   homepage = "HTTP/1.1 200 OK";
@@ -261,67 +304,71 @@ void flash() {
   flash(1, 100);
 }
 
+void set_rainbow(CRGB *colourArray, int numberToFill) {
+  static int index[2];
+  index[0] = numberToFill / 3;
+  index[1] = (numberToFill * 2) / 3;
+  fill_gradient_RGB(colourArray, 0, CRGB::Red, index[0] - 1, CRGB::Green);
+  fill_gradient_RGB(colourArray, index[0], CRGB::Green, index[1] - 1, CRGB::Blue);
+  fill_gradient_RGB(colourArray, index[1], CRGB::Blue, settings.colours_count - 1, CRGB::Red);
+}
+
+void setDefaultSettings() {
+  // Set default values
+  Serial.println("Setting default values");
+  settings.valid = SETTINGS_VALID;
+  settings.colours_count = NUM_LEDS;
+  settings.activeLEDs = NUM_LEDS;
+  settings.cycling = false;
+  settings.flashing = false;
+  //settings.password = ...;  // NB: Should consider encryption before adding this
+  settings.autoSave = true;
+  settings.brightness = 255;
+  set_rainbow(settings.colours_custom, settings.colours_count);
+}
+
+void saveSettings() {
+  EEPROM.put(SETTINGS_EEPROM_OFFSET, settings);
+  EEPROM.commit();
+  Serial.println("Settings saved to EEPROM");
+}
+
+void setup_EEPROM() {
+  int eepromSize = SETTINGS_EEPROM_OFFSET + sizeof(Settings);
+  EEPROM.begin(eepromSize);
+  // Check for obvious junk data in settings location, to handle as "No Settings"
+  byte firstByte;
+  EEPROM.get(SETTINGS_EEPROM_OFFSET, firstByte);
+  if (firstByte != 0x00 && firstByte != 0xFE) {
+    // The most obvious junk was not found, so attempt to load as settings
+    EEPROM.get(SETTINGS_EEPROM_OFFSET, settings);
+    // A specific value is set to show if this is valid, so read this to verify
+    // NB: If the structure changes, this SETTINGS_VALID constant should change to invalidate previous entries
+    if (settings.valid == SETTINGS_VALID) {
+      Serial.println("Loaded settings were valid");
+      flash(1, 500, CRGB::Green);
+    }
+    else {
+      Serial.println("Loaded settings were not valid");
+      flash(3, 250, CRGB::Red);
+    }
+  }
+  else {
+    Serial.println("No settings loaded");
+    flash(5, 200, CRGB::Orange);
+  }
+  if (settings.valid != SETTINGS_VALID) {
+    setDefaultSettings();
+  }
+}
+
 void setup() {
   Serial.begin(115200);
 
   Serial.println("\r\n\r\n**********************************\r\n\r\n");
 
   // Setup LED strip
-  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-
-  // Setup EEPROM access
-  /*
-    EEPROM.begin(4096);
-    byte data[4096];
-    EEPROM.get(0,data);
-    Serial.println("EEPROM Data:");
-    //Serial.println(data);
-    for(int i = 0; i < 4096; i++) {
-    Serial.print(data[i]);
-    }
-    Serial.println();
-    char hex[3];
-    for(int i = 0; i < 4096; i++) {
-    sprintf(hex, "%02X ", data[i]);
-    Serial.print(hex);
-    }
-    Serial.println();
-  */
-  int flashChipSize = ESP.getFlashChipSize();
-  Serial.print("flashChipSize=");
-  Serial.println(flashChipSize);
-  int eepromSize = SETTINGS_EEPROM_OFFSET + sizeof(Settings);
-  EEPROM.begin(eepromSize);
-  byte firstByte;
-  EEPROM.get(SETTINGS_EEPROM_OFFSET, firstByte);
-  if (firstByte != 0x00 && firstByte != 0xFE) {
-    EEPROM.get(SETTINGS_EEPROM_OFFSET, settings);
-    if (settings.valid == SETTINGS_VALID) {
-      Serial.println("Loaded settings were valid");
-      flash(1, 500, CRGB::Green);
-    } else {
-      Serial.println("Loaded settings were not valid");
-      flash(3, 250, CRGB::Red);
-    }
-  } else {
-    Serial.println("No settings loaded");
-    flash(5, 200, CRGB::Orange);
-  }
-  if (settings.valid != SETTINGS_VALID) {
-    // Set default values
-    Serial.println("Setting default values");
-    settings.valid = SETTINGS_VALID;
-    settings.colours_count = NUM_LEDS;
-    settings.activeLEDs = NUM_LEDS;
-    settings.cycling = false;
-    settings.flashing = false;
-    //settings.password = ...;  // NB: Should consider encryption before adding this
-    fill_rainbow(settings.colours_custom, settings.colours_count, CRGB::Red, CRGB::Blue);
-    // NB: This should be switched to a Save routine
-    EEPROM.put(SETTINGS_EEPROM_OFFSET, settings);
-    EEPROM.commit();
-    Serial.println("Settings saved to EEPROM");
-  }
+  FastLED.addLeds<NEOPIXEL, LED_DATA_PIN>(leds, NUM_LEDS);
 
   //WiFiManager (Local intialization - Once its business is done, there is no need to keep it around)
   WiFiManager wiFiManager;
@@ -343,23 +390,17 @@ void setup() {
 
   // Set up colour ranges
   colours_mixed[0] = CRGB::Blue;
-  //colours_mixed[0] = CRGB::DeepSkyBlue;
   colours_mixed[1] = CRGB::Green;
   colours_mixed[2] = CRGB::Red;
-  //colours_blue[0] = CRGB::Blue;
-  //colours_red[0] = CRGB::Red;
-  //colours_green[0] = CRGB::Green;
-  //colours_white[0] = CRGB::White;
-  //colours_black[0] = CRGB::Black;
-  //colours_purple[0] = CRGB::Purple;
-  //colours_purple[0] = CRGB::BlueViolet;
   colours_redgreen[0] = CRGB::Red;
   colours_redgreen[1] = CRGB::Green;
 
   Serial.println("Setting to LightYellow to show it is live");
   setLEDs(CRGB::LightYellow);
 
+#ifdef USE_ARDUINO_OTA
   setup_ArduinoOTA();
+#endif	// USE_ARDUINO_OTA
 
   buildHomepage();
 
@@ -367,10 +408,12 @@ void setup() {
   Serial.print("HTTP server started - http://");
   Serial.println(WiFi.localIP());
 
+#ifdef USE_BLYNK
   // Starting Blynk
   //Blynk.begin(blynkAuthToken, WiFi.SSID(),
   Blynk.config(blynkAuthToken);
   //Blynk.begin(blynkAuthToken, WiFi.SSID().c_str(), WiFi.psk().c_str());
+#endif // USE_BLYNK
 }
 
 void returnJSON(WiFiClient client, String json) {
@@ -388,14 +431,8 @@ void returnHomepage(WiFiClient client) {
   delay(1);
 }
 
-const int STATE_OFF = 0;
-const int STATE_ON = 1;
 bool started = false;
-int state = STATE_OFF;
-int customLength = 1;
-CRGB colours_custom[NUM_COLOURS];
-bool cycling = false;
-bool flashing = false;
+int myVal = testVal;
 
 void update(int command) {
   Serial.print("In update: command=");
@@ -404,7 +441,9 @@ void update(int command) {
     case COMMAND_NONE:
       break;
     case COMMAND_OFF:
-      setLEDs(CRGB::Black);
+      //setLEDs(CRGB::Black);
+      settings.brightness = 0;
+      setLEDs();
       break;
     /* NB: Not currently storing last value, can be re-added when that is available
       case COMMAND_ON:
@@ -438,22 +477,22 @@ void update(int command) {
       setLEDs(3, colours_mixed);
       break;
     case COMMAND_CUSTOM_SINGLE:
-      setLEDs(colours_custom[0]);
+      setLEDs(settings.colours_custom[0]);
       break;
     case COMMAND_CUSTOM_MIXED:
-      setLEDs(customLength, colours_custom);
+      setLEDs(settings.colours_count, settings.colours_custom);
       break;
     case COMMAND_FLASH_ON:
-      flashing = true;
+      settings.flashing = true;
       break;
     case COMMAND_FLASH_OFF:
-      flashing = false;
+      settings.flashing = false;
       break;
     case COMMAND_CYCLE_ON:
-      cycling = true;
+      settings.cycling = true;
       break;
     case COMMAND_CYCLE_OFF:
-      cycling = false;
+      settings.cycling = false;
       break;
     case COMMAND_REMOVE_LED:
       removeLED();
@@ -461,35 +500,49 @@ void update(int command) {
     case COMMAND_ADD_LED:
       addLED();
       break;
+    case COMMAND_SET_BRIGHTNESS:
+      FastLED.setBrightness(settings.brightness);
+      break;
+    case COMMAND_DARKEN:
+      settings.brightness = settings.brightness >= 8 ? settings.brightness - 8 : 0;
+      update(COMMAND_SET_BRIGHTNESS);
+    case COMMAND_LIGHTEN:
+      settings.brightness = settings.brightness < 247 ? settings.brightness + 8 : 255;
+      update(COMMAND_SET_BRIGHTNESS);
     case COMMAND_SAVE:
-      Serial.println("COMMAND_SAVE has not been implemented yet");
+      Serial.println("COMMAND_SAVE: Saving settings to EEPROM");
+      saveSettings();
+#ifdef USE_BLYNK
       terminal.println("COMMAND_SAVE has not been implemented yet");
+#endif	// USE_BLYNK
       break;
     case COMMAND_LOAD:
-      Serial.println("COMMAND_LOAD has not been implemented yet");
+      //Serial.println("COMMAND_LOAD has not been implemented yet");
+      Serial.println("COMMAND_LOAD: Load settings from EEPROM");
+#ifdef USE_BLYNK
       terminal.println("COMMAND_LOAD has not been implemented yet");
+#endif	// USE_BLYNK
       break;
     case COMMAND_RAINBOW:
-      //Serial.println("COMMAND_RAINBOW has not been implemented yet");
       Serial.println("Setting a rainbow");
-      //fill_rainbow(settings.colours_custom, settings.colours_count, CRGB::Red, CRGB::Blue);
-      /*
-      fill_rainbow(settings.colours_custom, settings.colours_count / 2, CRGB::Red, CRGB::Cyan);
-      fill_rainbow(settings.colours_custom[startIndex], settings.colours_count - startIndex, CRGB::Cyan, CRGB::Red);
-      //*/
-      //static int midIndex = settings.colours_count / 2;
-      //fill_gradient_RGB(settings.colours_custom, 0, CRGB::Red, midIndex - 1, CRGB::Cyan);
-      //fill_gradient_RGB(settings.colours_custom, midIndex, CRGB::Cyan, settings.colours_count - 1, CRGB::Red);
-      static int index[2];
-      index[0] = settings.colours_count / 3;
-      index[1] = (settings.colours_count * 2) / 3;
-      fill_gradient_RGB(settings.colours_custom, 0, CRGB::Red, index[0]-1, CRGB::Green);
-      fill_gradient_RGB(settings.colours_custom, index[0], CRGB::Green, index[1]-1, CRGB::Blue);
-      fill_gradient_RGB(settings.colours_custom, index[1], CRGB::Blue, settings.colours_count-1, CRGB::Red);
+      set_rainbow(settings.colours_custom, settings.colours_count);
       setLEDs(settings.colours_count, settings.colours_custom);
       break;
     case COMMAND_CUSTOM_RAINBOW:
       Serial.println("Received custom rainbow command, but not currently implemented");
+      break;
+    case COMMAND_RESET:
+      Serial.println("COMMAND_RESET ... resetting");
+      ESP.reset();
+      break;
+    case COMMAND_RESTART:
+      Serial.println("COMMAND_RESTART ... restarting");
+      ESP.restart();
+      break;
+    case COMMAND_CLEAR_WIFI:
+      Serial.println("COMMAND_CLEAR_WIFI ... clearing wifi settings");
+      WiFiManager wiFiManager;
+      wiFiManager.resetSettings();
       break;
   }
 }
@@ -611,7 +664,12 @@ void handleWiFiClient()
 }
 
 void loop() {
+
+#ifdef USE_ARDUINO_OTA
   ArduinoOTA.handle();
+#endif	// USE_ARDUINO_OTA
+
+#ifdef USE_BLYNK
   Blynk.run();
 
   if (Blynk.connected()) {
@@ -633,24 +691,29 @@ void loop() {
       terminal.flush();
     }
   }
+#endif	// USE_BLYNK
+
+#ifdef USE_MQTT
+#endif	// USE_MQTT
 
   handleWiFiClient();
 }
 
+#ifdef USE_BLYNK
 /*
-  BLYNK_CONNECTED()
-  {
+BLYNK_CONNECTED()
+{
 	// Refetch/set all Blynk pin values
 	Blynk.syncAll();
-  }
-  //*/
+}
+//*/
 
 BLYNK_DISCONNECTED()
 {
   started = false;
 }
 
-BLYNK_WRITE(0)
+BLYNK_WRITE(OFF_PIN)
 {
   Serial.print("Virtual Button 0 pressed");
   int value = param.asInt();
@@ -661,7 +724,6 @@ BLYNK_WRITE(0)
     update(COMMAND_OFF);
   }
 }
-
 BLYNK_WRITE(TERMINAL_PIN)
 {
   String value = param.asStr();
@@ -683,3 +745,86 @@ BLYNK_WRITE(RGB_PIN)
   setLEDs(newColour);
 }
 
+BLYNK_WRITE(MENU_PIN)
+{
+  switch (param.asInt()) {
+    case BLYNK_MENU_OFF:
+      update(COMMAND_OFF);
+      break;
+    case BLYNK_MENU_WHITE:
+      update(COMMAND_WHITE);
+      break;
+    case BLYNK_MENU_RAINBOW:
+      update(COMMAND_RAINBOW);
+      break;
+    case BLYNK_MENU_RED:
+      update(COMMAND_RED);
+      break;
+    case BLYNK_MENU_GREEN:
+      update(COMMAND_GREEN);
+      break;
+    case BLYNK_MENU_BLUE:
+      update(COMMAND_BLUE);
+      break;
+    case BLYNK_MENU_MIXED:
+      update(COMMAND_MIXED);
+      break;
+    case BLYNK_MENU_RED_GREEN:
+      update(COMMAND_REDGREEN);
+      break;
+    case BLYNK_MENU_RANDOM_RAINBOW:
+      //update(COMMAND_RANDOM_RAINBOW);
+      Serial.println("BLYNK_MENU_RANDOM_RAINBOW received, but currently not implemented");
+      break;
+    case BLYNK_MENU_RANDOM_RGB:
+      //update(COMMAND_RANDOM_RAINBOW);
+      Serial.println("BLYNK_MENU_RANDOM_RGB received, but currently not implemented");
+      break;
+    case BLYNK_MENU_SAVE:
+      update(COMMAND_SAVE);
+      break;
+    case BLYNK_MENU_LOAD:
+      update(COMMAND_LOAD);
+      break;
+    case BLYNK_MENU_RESTART:
+      update(COMMAND_RESTART);
+      break;
+    case BLYNK_MENU_RESET:
+      update(COMMAND_RESET);
+      break;
+    case BLYNK_MENU_CLEAR_WIFI:
+      update(COMMAND_CLEAR_WIFI);
+      break;
+    default:
+      Serial.print("Unhandled Blynk menu item: ");
+      Serial.println(param.asInt());
+      break;
+  }
+}
+
+BLYNK_WRITE(RESTART_PIN)
+{
+  Serial.println("BLYNK: RESTART_PIN pressed");
+  update(COMMAND_RESTART);
+}
+
+// NB: LED_PIN is an output pin
+//BLYNK_WRITE(LED_PIN) { }
+
+BLYNK_WRITE(BRIGHTNESS_PIN)
+{
+  int brightness = param.asInt();
+  Serial.print("BLYNK: Brightness received: ");
+  Serial.println(brightness);
+  settings.brightness = brightness;
+  update(COMMAND_SET_BRIGHTNESS);
+}
+
+BLYNK_WRITE(LIGHT_SENSOR_PIN)
+{
+  int lux = param.asInt();
+  Serial.print("BLYNK: Light sensor value received: ");
+  Serial.println(lux);
+}
+//*/
+#endif	// USE_BLYNK
